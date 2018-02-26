7812ffa352c2c12c8803942f7d933905df7d6166
xiaokang pu <pxk@seiue.com>
2018-02-26 19:54:38 +0800

    build

---
+++ dist/index.js
@@ -0,0 +1,56 @@
+"use strict";
+exports.__esModule = true;
+var Promise = /** @class */ (function () {
+    /**
+     * create a Promise execute
+     */
+    function Promise(execute) {
+        this.status = 'pending';
+        this.resolves = [];
+        this.rejects = [];
+        var onFulfilled = this.fulfilled.bind(this);
+        var onRejected = this.rejected.bind(this);
+        try {
+            execute(onFulfilled, onRejected);
+        }
+        catch (e) {
+            onRejected(e);
+        }
+    }
+    Promise.prototype.then = function (resolve, reject) {
+        this.resolves.push(resolve);
+        if (reject) {
+            this.rejects.push(reject);
+        }
+        if (this.status === 'rejected') {
+            this.callback(this.rejects);
+        }
+        else if (this.status === 'fulfilled') {
+            this.callback(this.resolves);
+        }
+    };
+    Promise.prototype["catch"] = function (reject) {
+        this.rejects.push(reject);
+    };
+    Promise.prototype.fulfilled = function (result) {
+        this.status = 'fulfilled';
+        this.result = result;
+        this.callback(this.resolves);
+    };
+    Promise.prototype.rejected = function (reason) {
+        this.status = 'rejected';
+        this.result = reason;
+        this.callback(this.rejects);
+    };
+    Promise.prototype.callback = function (callbacks) {
+        var callback;
+        while (callback = callbacks.shift()) {
+            callback(this.result);
+        }
+    };
+    Promise.all = function (promises) {
+        console.log(promises);
+    };
+    return Promise;
+}());
+exports["default"] = Promise;
